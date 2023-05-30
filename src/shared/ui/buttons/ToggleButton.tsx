c773941215b46517f78932208f722d580ab2da79
Sergey Rovkach <srovk22@gmail.com>
2023-05-30 16:42:52 +0900

    fix: radio button checked

--- src/shared/ui/buttons/ToggleButton.tsx
+++ src/shared/ui/buttons/ToggleButton.tsx
@@ -13,7 +13,7 @@ export const ToggleButton = ({
   checked: boolean;
 }) => (
   <div role="button">
-    <input onChange={onChange} type="radio" id={value} name="tabs" className="hidden" checked={checked} />
+    <input onChange={onChange} type="radio" id={value} name="tabs" className="hidden" value={value} />
     <label htmlFor={value} className={`transition duration-150 cursor-pointer flex items-center justify-center truncate uppercase select-none text-sm font-medium rounded-full p-4 py-2 ${checked ? "text-white bg-mainColor hover:bg-hoverColorBg" : "text-gray-500 bg-transparent border hover:text-gray-400"}`}>
       {label}
     </label>
